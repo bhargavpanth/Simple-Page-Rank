2c50c771f4016c409259da4655f1d00c3c36e299
Bhargav Panth <bhargavrpanth@gmail.com>
2018-04-23 14:59:54 +0100

    santiy checks

--- test/test.py
+++ test/test.py
@@ -3,11 +3,12 @@ sys.path.append('../src')
 from page_rank import PageRank
 import unittest
 
+
 class TestPageRankSanity(unittest.TestCase):
 
-	def test_object_creation(self):
+	def test_objectCreation(self):
 		test = PageRank('http://github.com/')
-		print type(test)
+		self.assertTrue(type(test) is instance)
 
 
 if __name__ == '__main__':
