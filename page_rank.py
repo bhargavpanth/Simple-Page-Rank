6564c09933e609004c91681abc5222a5e7501c1a
Bhargav Panth <bhargavrpanth@gmail.com>
2018-04-23 13:23:12 +0100

    first commit

---
+++ page_rank.py
@@ -0,0 +1,14 @@
+import urllib
+
+class PageRank:
+	"""docstring for PageRank"""
+	def __init__(self, arg):
+		self.arg = arg
+
+	def calculate_outgoing_links():
+		pass
+
+	def calculate_incoming_links():
+		pass
+
+		
