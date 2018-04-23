b367cc76f811276d0cb9272707461f506fc58b89
Bhargav Panth <bhargavrpanth@gmail.com>
2018-04-23 14:04:25 +0100

    scaffolding done

--- src/page_rank.py
+++ src/page_rank.py
@@ -1,14 +1,44 @@
 import urllib
+from bs4 import BeautifulSoup
+
 
 class PageRank:
+
 	"""docstring for PageRank"""
-	def __init__(self, arg):
-		self.arg = arg
+	
+	def __init__(self, url):
+		self.url = url
 
-	def calculate_outgoing_links():
-		pass
 
-	def calculate_incoming_links():
+	def request(self):
+		'''
+		calculate the outgoing links from the current URL provided
+		'''
+		try:
+			response = urllib.request(self.url)
+		except Exception as e:
+			raise
+		else:
+			if response.getcode() == 200:
+				# pass the response to outgoing links
+				self.calculate_outgoing_links(response)
+			else:
+				print 'web content was not retrieved'
+				break
+		finally: 
+			pass
+
+
+	def calculate_outgoing_links(self, response):
+		# parse the content with BS4
+
+
+
+	def calculate_incoming_links(self):
+		'''
+		provide incoming link numbers to each of the websites
+		'''
+
 		pass
 
 		
