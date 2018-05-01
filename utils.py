1604652bc4782526e7137f9fc21aa181fb90e31b
Bhargav Panth <bhargavrpanth@gmail.com>
2018-05-01 22:51:37 +0100

    small changes to utils

--- utils.py
+++ utils.py
@@ -15,17 +15,25 @@ class Utilities:
 	def parse_content(self, url):
 		# ensure url is utf-8 encoded
 		try:
-			content = urllib.request(url.encode('utf-8'))
+			response = urllib.urlopen(url.encode('utf-8'))
 		except Exception as e:
 			raise
 		else:
 			if response.getcode() == 200:
-				# pass the response to outgoing links
 				return response
 			else:
 				print 'web content was not retrieved'
-				break
-		finally: 
+				pass
+		finally:
 			pass
 
+"""
+def main():
+	test = Utilities()
+	resp = test.parse_content('http://github.com')
+	print resp
+
+if __name__ == '__main__':
+	main()
+"""
 	
